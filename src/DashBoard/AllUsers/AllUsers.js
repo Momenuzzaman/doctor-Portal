ff67cde39effe64e291b61124e05dc74cfe65aa4
mdemon420 <mdeamon4@gmail.com>
2023-04-07 16:14:49 +0600

    show all user

--- src/DashBoard/AllUsers/AllUsers.js
+++ src/DashBoard/AllUsers/AllUsers.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { useQuery } from 'react-query';
 
 const AllUsers = () => {
+
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users')
+            const data = res.json()
+            return data;
+        }
+    });
     return (
-        <div>AllUsers</div>
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Favorite Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, i) => {
+                            const { name, email } = user
+                            return <tr>
+                                <th>{i + 1}</th>
+                                <td>{name}</td>
+                                <td>{email}</td>
+                                <td>Blue</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 
