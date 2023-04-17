b245a7c12253bea1037111eaa2587937efe7c6a9
mdemon420 <mdeamon4@gmail.com>
2023-04-17 21:25:51 +0600

    create doctor form

--- src/DashBoard/AddDoctor/AddDoctor.js
+++ src/DashBoard/AddDoctor/AddDoctor.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 const AddDoctor = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+
+    const handleAddDoctor = (data) => {
+        console.log(data);
+    }
     return (
-        <div>AddDoctor</div>
+        <div className="mt-10 h-[550px] w-[370px] md:w-[390px]  mx-auto flex justify-center items-center shadow-xl rounded-md">
+            <div>
+                <h1 className='text-xl text-center font-bold'>Add Doctor</h1>
+                <form onSubmit={handleSubmit(handleAddDoctor)}>
+                    <div className="form-control w-[330px] mt-[20px]">
+                        <label className="label">
+                            <span className="label-text font-semibold">Name</span>
+                        </label>
+                        <input type="text" {
+                            ...register("name",
+                                {
+                                    required: "Name is required"
+                                })}
+                            className="input input-bordered w-full " />
+                        {errors.text && <p className='text-red-500'>{errors.text?.message}</p>}
+                    </div>
+                    <div className="form-control w-[330px] mt-[5px]">
+                        <label className="label">
+                            <span className="label-text font-semibold">Email</span>
+                        </label>
+                        <input type="email" {
+                            ...register("email",
+                                {
+                                    required: "Email Address is required"
+                                })}
+                            className="input input-bordered w-full " />
+                        {errors.email && <p className='text-red-500'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control  w-[330px]">
+                        <label className="label">
+                            <span className="label-text font-semibold">Password</span>
+                        </label>
+                    </div>
+                    <input type="submit" className='w-full btn btn-accent' value="Add Doctor" />
+                </form>
+            </div>
+
+        </div>
     )
 }
 
